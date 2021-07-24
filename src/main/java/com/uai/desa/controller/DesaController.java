8254ab6a7566779fb1ce2d39b23111bfbb337a14
luq-uai <87761905+luq-uai@users.noreply.github.com>
2021-07-24 02:39:26 -0300

    Update DesaController.java

--- src/main/java/com/uai/desa/controller/DesaController.java
+++ src/main/java/com/uai/desa/controller/DesaController.java
@@ -8,11 +8,15 @@ public class DesaController {
 
 	@RequestMapping(value = "/")
 	public String teste() {
-		
-		String html = "<h1>TP de Diseño y Arquitectura de Software</h1>";
-		html = html + "<h2>Lucas Freire</h2>"; 
+		StringBuilder html = new StringBuilder();
+		html.append("<h1>TP de Diseño y Arquitectura de Software</h1>");
+		html.append("<h2>Christian Chamula</h2>"); 
+		html.append("<h2>Lorena Fuentes</h2>"); 
+		html.append("<h2>Lucas Freire</h2>"); 
+		html.append("<h2>Mariano Boucher</h2>"); 
+		html.append("<h2>Fernando Coronilla</h2>"); 
 			
-		return html;
+		return html.toString();
 	}
 
 }
